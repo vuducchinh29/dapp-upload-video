56ccd4abd5efadd3d1d291c4320c149beb1a9dbd
vuducchinh29 <>
2020-11-10 17:11:05 +0700

    render identicon for account avatar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import dvideo from '../dvideo.png'
 class Navbar extends Component {
 
   render() {
+    const { account } = this.props
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow text-monospace">
         <a
@@ -19,9 +20,16 @@ class Navbar extends Component {
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
-              <small id="account">{this.props.account}</small>
+              <small id="account">{account}</small>
             </small>
-              {/* Return Account&Identicon... */}
+            {account!== '0x0' ?
+            <img
+              className="ml-2"
+              width="30"
+              height="30"
+              src={`data:image/png;base64,${new Identicon(account, 30).toString()}`}
+              alt=""
+            /> : <span></span>}
           </li>
         </ul>
       </nav>
